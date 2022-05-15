5b86eea0da9b120b91455065ddfcdfa63aa23985
DENIS <denis@inplease.co>
2022-05-15 17:39:23 -0500

    mensaje a enviar

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -1,49 +1,38 @@
 import React, { useState } from "react";
 import TaskForm from "./TaskForm";
 import Task from "./Task";
-import '../components/Style-sheet/TasksList.css';
+import "../components/Style-sheet/TasksList.css";
 
+function TasksList() {
+  const [tasks, setTasks] = useState([]);
 
-
-function TasksList( ) {  
-    
-    const [tasks, setTasks] = useState([]);
-
-  
-    const addTask = task => {
-       
-       if(task.texto.trim()) { 
-           task.texto = task.texto.trim();
-           const tasksUpdate =[task,...tasks];
-            setTasks(tasksUpdate);
-       }
+  const addTask = (task) => {
+    if (task.texto.trim()) {
+      task.texto = task.texto.trim();
+      const tasksUpdate = [task, ...tasks];
+      setTasks(tasksUpdate);
     }
-    const eliminarTarea = id =>{
-        const tasksUpdate = tasks.filter(task => task.id !== id);
-        setTasks(tasksUpdate);
-    }
-    return(
-        <>
-          <TaskForm onSubmit={addTask} />
-          <div className="tasks-list-container"> 
-           {
-            tasks.map((task) =>
-                <Task 
-                key={task.id}
-                id={task.id}
-                texto ={task.texto} 
-                time={task.time}
-                eliminarTarea ={eliminarTarea}
-                />
-           
-               
-            )
-           }
-          </div>
-
-         
-        </>
-    );
+  };
+  const eliminarTarea = (id) => {
+    const tasksUpdate = tasks.filter((task) => task.id !== id);
+    setTasks(tasksUpdate);
+  };
+  return (
+    <>
+      <TaskForm onSubmit={addTask} />
+      <div className="tasks-list-container">
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            id={task.id}
+            texto={task.texto}
+            time={task.time}
+            eliminarTarea={eliminarTarea}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default TasksList;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,34 +1,30 @@
-import React, { Profiler } from "react";
+import React from "react";
 import { MenuItems } from "./MenuItems";
-import { ProfileItem } from "./Profile"
-
-
+import { ProfileItem } from "./Profile";
 
 export default function Navbar() {
   return (
-    <nav  className="NavbarItems">
-     
-      <div className="AllNavbar">{ProfileItem.map((item, index) => {
-           return (
-              <li key={index}>
-               <a className={item.cName} href={item.url} >
-                  {item.Title}
-                </a>
-              </li>
-           );
-         })}
-         {MenuItems.map((item, index) => {
-           return (
-              <li key={index}>
-               <a className={item.cName} href={item.url} >
-                  {item.Title}
-                </a>
-              </li>
-           ); })}
-      
+    <nav className="NavbarItems">
+      <div className="AllNavbar">
+        {ProfileItem.map((item, index) => {
+          return (
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
+                {item.Title}
+              </a>
+            </li>
+          );
+        })}
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
+                {item.Title}
+              </a>
+            </li>
+          );
+        })}
       </div>
-       
     </nav>
-   
   );
 }
